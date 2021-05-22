dcc60e27fc72d0f8cfdf9583fc45c4a94fc4df8d
Farzana Bashar <farzana@Farzanas-MacBook-Pro.local>
2021-05-22 16:32:01 +0600

    edit index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express= require("express");
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000;
+const port = process.env.PORT || 3000;
+// const getId = require('docker-container-id');
 
 app.use(bodyParser.json());
 
-app.get('/api/health', (req, res) => {
-    res.json({'message': 'You are healthy'});
+app.get('/', (req, res) => {
+    // res.json(`you are listening to port ${port}`);
+    res.json({'message': `You are healthy and container id is ${process.env.APPID}`});
   });
 
 app.listen(port, () => {
